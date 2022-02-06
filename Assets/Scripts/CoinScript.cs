612985df1f1c2705fb38c653d7c9c9c123caba2b
33_AimanEzrafil <phoenexus2107@gmail.com>
2022-02-06 12:04:10 +0800

    Particle effects for coin

---
+++ Assets/Scripts/CoinScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinScript : MonoBehaviour
+{
+    public int rotateSpeed;
+
+    public Transform explosion;
+
+    void Update()
+    {
+        transform.Rotate(0, rotateSpeed, 0, Space.World);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            GameObject exploder = ((Transform)Instantiate(explosion, this.transform.position,
+                this.transform.rotation)).gameObject;
+            Destroy(exploder, 2.0f);
+        }
+    }
+}
